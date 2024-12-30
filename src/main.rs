bcca449379e3539bb4a7906dd12cbe2f7aedee2c
Andre Masella <andre@masella.name>
2024-12-30 17:01:08 -0500

    Move file scanning into separate module
    Transfers all the file scanning and watching into a separate module.

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,12 @@ mod decoder;
 mod encoder;
 mod exit_filter;
 mod playlist;
+mod scanner;
 
 use crate::decoder::DecodedStream;
 use crate::encoder::EncodedStream;
 use crate::exit_filter::ExitFilter;
 use crate::playlist::Playlist;
-use async_watcher::notify::{Error, ErrorKind};
-use async_watcher::{
-    AsyncDebouncer, DebouncedEvent,
-    notify::{EventKind, RecursiveMode},
-};
 use clap::Parser;
 use futures::future::BoxFuture;
 use futures::{FutureExt, StreamExt};
@@ -26,11 +22,8 @@ use std::convert::Infallible;
 use std::net::{IpAddr, Ipv6Addr, SocketAddr};
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
-use std::time::Duration;
 use tokio::net::TcpListener;
-use tokio::signal::unix::SignalKind;
 use tokio::sync::{RwLock, broadcast};
-use walkdir::WalkDir;
 
 type SongList = Arc<RwLock<BTreeSet<Arc<Path>>>>;
 #[derive(Parser)]
@@ -40,27 +33,6 @@ struct Arguments {
     port: u16,
     #[arg(value_name = "DIRECTORY")]
     path: PathBuf,
-}
-fn scan(path: &Path) -> BTreeSet<Arc<Path>> {
-    let result: BTreeSet<Arc<Path>> = WalkDir::new(path)
-        .follow_links(true)
-        .into_iter()
-        .filter_map(|entry| match entry {
-            Err(e) => {
-                eprintln!("Failed searching files: {}", e);
-                None
-            }
-            Ok(entry) => {
-                if entry.file_type().is_file() {
-                    Some(entry.into_path().into())
-                } else {
-                    None
-                }
-            }
-        })
-        .collect();
-    eprintln!("Scanned {} files", result.len());
-    result
 }
 
 #[derive(Clone)]
@@ -118,98 +90,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         port,
         path: root_path,
     } = Arguments::parse();
-    let songs = Arc::new(RwLock::new(scan(&root_path)));
-    let (exit_tx, mut exit_rx) = tokio::sync::broadcast::channel(1);
-
-    {
-        let songs = songs.clone();
-        let mut exit_rx = exit_tx.subscribe();
-        let (mut debouncer, mut file_events) =
-            AsyncDebouncer::new_with_channel(Duration::from_secs(1), Some(Duration::from_secs(1)))
-                .await?;
-        debouncer
-            .watcher()
-            .watch(&root_path, RecursiveMode::Recursive)
-            .expect("Failed to scan directory");
-        tokio::spawn(async move {
-            use async_watcher::notify::Error as NotifyError;
-            enum WatcherEvent {
-                Files(Option<Result<Vec<DebouncedEvent>, Vec<NotifyError>>>),
-                Exit,
-                Rescan,
-            }
-            let Ok(hup) = tokio::signal::unix::signal(SignalKind::hangup()) else {
-                eprintln!("Couldn't bind to SIGHUP");
-                return;
-            };
-            let mut hup = Box::pin(hup);
-            loop {
-                let event = tokio::select! {
-                    e = file_events.recv() => WatcherEvent::Files(e),
-                    _ = exit_rx.recv() => WatcherEvent::Exit,
-                    _ = hup.recv() => WatcherEvent::Rescan,
-                };
-                match event {
-                    WatcherEvent::Exit | WatcherEvent::Files(None) => break,
-                    WatcherEvent::Rescan => {
-                        let mut songs = songs.write().await;
-                        *songs = scan(&root_path);
-                    }
-                    WatcherEvent::Files(Some(Ok(events))) => {
-                        let mut songs = songs.write().await;
-                        for DebouncedEvent { path, event, .. } in events {
-                            match event.kind {
-                                EventKind::Any => {}
-                                EventKind::Access(_) => {}
-                                EventKind::Create(_) => {
-                                    songs.insert(path.into());
-                                }
-                                EventKind::Modify(_) => {}
-                                EventKind::Remove(_) => {
-                                    songs.remove(path.as_path());
-                                }
-                                EventKind::Other => {
-                                    eprintln!(
-                                        "Got an other event for {}. Triggering rescan.",
-                                        path.display()
-                                    );
-                                    *songs = scan(&root_path)
-                                }
-                            }
-                        }
-                    }
-                    WatcherEvent::Files(Some(Err(errors))) => {
-                        for Error { kind, paths } in errors {
-                            match kind {
-                                ErrorKind::Generic(e) => {
-                                    eprintln!("Error watching files: {}", e);
-                                }
-                                ErrorKind::Io(e) => {
-                                    eprintln!("Error watching files: {}", e)
-                                }
-                                ErrorKind::PathNotFound => {
-                                    let mut songs = songs.write().await;
-                                    for path in paths {
-                                        songs.remove(path.as_path());
-                                    }
-                                }
-                                ErrorKind::WatchNotFound => {
-                                    eprintln!("Watch not found");
-                                }
-                                ErrorKind::InvalidConfig(_) => {
-                                    eprintln!("Invalid config");
-                                }
-                                ErrorKind::MaxFilesWatch => {
-                                    eprintln!("Watching too many files")
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        });
-    }
-
+    let (exit_tx, mut exit_rx) = broadcast::channel(1);
+    let songs = scanner::create_scanner(root_path, &exit_tx).await?;
     let songs = Songs {
         songs,
         exit: exit_tx.clone(),
