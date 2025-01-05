77236b09c478b782d116f5a2f6a734bfcee894c2
Andre Masella <andre@masella.name>
2025-01-04 20:56:28 -0500

    Reduce buffer copy where possible
    This tries to allocate buffers based only on used capacity. The symphonia
    convert function doesn't permit this.

--- src/decoder.rs
+++ src/decoder.rs
@@ -6,11 +6,13 @@ use std::path::Path;
 use std::pin::Pin;
 use std::sync::Arc;
 use std::task::{Context, Poll};
-use symphonia::core::audio::{AudioBuffer, Channels, SignalSpec};
+use symphonia::core::audio::{AudioBuffer, AudioBufferRef, Channels, Signal, SignalSpec};
 use symphonia::core::codecs::Decoder;
+use symphonia::core::conv::IntoSample;
 use symphonia::core::formats::Track;
 use symphonia::core::io::MediaSourceStream;
 use symphonia::core::probe::ProbeResult;
+use symphonia::core::sample::Sample;
 
 pub enum DecodedStream {
     Empty,
@@ -70,44 +72,73 @@ impl Stream for DecodedStream {
         else {
             return Poll::Ready(None);
         };
-        let Ok(packet) = prober.format.next_packet() else {
-            eprintln!("Bad packet in {}", song.display());
-            return Poll::Ready(None);
-        };
-        let mut current_track_temp = None;
-        swap(&mut current_track_temp, current_track);
-        let (mut decoder, track_id) =
-            match current_track_temp.filter(|(_, track_id)| packet.track_id() == *track_id) {
-                None => match tracks.pop_front() {
-                    None => return Poll::Ready(None),
-                    Some(track) => {
-                        let Ok(decoder) = symphonia::default::get_codecs()
-                            .make(&track.codec_params, &Default::default())
-                        else {
-                            eprintln!("Bad track in {}", song.display());
-                            return Poll::Ready(None);
-                        };
-                        (decoder, track.id)
-                    }
-                },
-                Some(value) => value,
+        loop {
+            let Ok(packet) = prober.format.next_packet() else {
+                eprintln!("Bad packet in {}", song.display());
+                return Poll::Ready(None);
             };
+            let mut current_track_temp = None;
+            swap(&mut current_track_temp, current_track);
+            let (mut decoder, track_id) =
+                match current_track_temp.filter(|(_, track_id)| packet.track_id() == *track_id) {
+                    None => match tracks.pop_front() {
+                        None => return Poll::Ready(None),
+                        Some(track) => {
+                            let Ok(decoder) = symphonia::default::get_codecs()
+                                .make(&track.codec_params, &Default::default())
+                            else {
+                                eprintln!("Bad track in {}", song.display());
+                                return Poll::Ready(None);
+                            };
+                            (decoder, track.id)
+                        }
+                    },
+                    Some(value) => value,
+                };
 
-        let result = match decoder.decode(&packet) {
-            Err(e) => {
-                eprintln!("Decode error in {}: {}", song.display(), e);
-                Poll::Ready(None)
+            let result = match decoder.decode(&packet) {
+                Err(e) => {
+                    eprintln!("Decode error in {}: {}", song.display(), e);
+                    Some(Poll::Ready(None))
+                }
+                Ok(data) => {
+                    if data.frames() == 0 {
+                        None
+                    } else {
+                        Some(Poll::Ready(Some(match data {
+                            AudioBufferRef::U8(buffer) => stingy_copy(buffer.as_ref()),
+                            AudioBufferRef::U16(buffer) => stingy_copy(buffer.as_ref()),
+                            AudioBufferRef::U24(buffer) => stingy_copy(buffer.as_ref()),
+                            AudioBufferRef::U32(buffer) => stingy_copy(buffer.as_ref()),
+                            AudioBufferRef::S8(buffer) => stingy_copy(buffer.as_ref()),
+                            AudioBufferRef::S16(buffer) => stingy_copy(buffer.as_ref()),
+                            AudioBufferRef::S24(buffer) => stingy_copy(buffer.as_ref()),
+                            AudioBufferRef::S32(buffer) => stingy_copy(buffer.as_ref()),
+                            AudioBufferRef::F32(buffer) => stingy_copy(buffer.as_ref()),
+                            AudioBufferRef::F64(buffer) => stingy_copy(buffer.as_ref()),
+                        })))
+                    }
+                }
+            };
+            *current_track = Some((decoder, track_id));
+            if let Some(result) = result {
+                break result;
+            } else {
+                continue;
             }
-            Ok(data) => {
-                let mut output_buffer = AudioBuffer::<i16>::new(
-                    data.capacity() as u64,
-                    SignalSpec::new(44100, Channels::FRONT_LEFT | Channels::FRONT_RIGHT),
-                );
-                data.convert(&mut output_buffer);
-                Poll::Ready(Some(output_buffer))
-            }
-        };
-        *current_track = Some((decoder, track_id));
-        result
+        }
     }
+}
+fn stingy_copy<T: Sample + IntoSample<i16>>(input: &AudioBuffer<T>) -> AudioBuffer<i16> {
+    let mut output = AudioBuffer::<i16>::new(
+        input.frames() as u64,
+        SignalSpec::new(44100, Channels::FRONT_LEFT | Channels::FRONT_RIGHT),
+    );
+    output.render_reserved(Some(input.frames()));
+    for channel in 0..2 {
+        for (dest, src) in output.chan_mut(channel).iter_mut().zip(input.chan(channel)) {
+            *dest = (*src).into_sample();
+        }
+    }
+    output
 }
