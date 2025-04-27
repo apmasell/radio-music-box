2552ddaa2fda7d010ff65a73b924adfaeac7b3aa
Andre Masella <andre@masella.name>
2025-04-27 13:03:57 -0400

    Define custom rate limiter
    Just create my own token bucket to allow overruns.

--- src/rate_limited_stream.rs
+++ src/rate_limited_stream.rs
@@ -2,14 +2,14 @@ use futures::{FutureExt, Stream};
 use hyper::body::Bytes;
 use std::pin::Pin;
 use std::task::{Context, Poll};
-use std::time::Duration;
+use std::time::{Duration, SystemTime};
 use symphonia::core::audio::{AudioBuffer, Signal};
-use tokenbucket::TokenBucket;
 use tokio::time::Sleep;
 
 pub struct RateLimitedStream<S> {
     stream: S,
-    bucket: TokenBucket,
+    tokens: i128,
+    last_checked: SystemTime,
     sleep: Option<Pin<Box<Sleep>>>,
 }
 
@@ -20,26 +20,27 @@ where
     pub fn new(stream: S) -> Self {
         Self {
             stream,
-            bucket: TokenBucket::new(S::Item::RATE, S::Item::RATE * 10.0),
+            tokens: (S::Item::RATE * 1_000) as i128,
+            last_checked: SystemTime::now(),
             sleep: None,
         }
     }
 }
 pub trait Rated {
-    const RATE: f64;
-    fn quantity(&self) -> f64;
+    const RATE: u64;
+    fn quantity(&self) -> u64;
 }
 impl Rated for Bytes {
-    const RATE: f64 = 100_000.0;
-    fn quantity(&self) -> f64 {
-        self.len() as f64
+    const RATE: u64 = 100_000;
+    fn quantity(&self) -> u64 {
+        self.len() as u64
     }
 }
 impl Rated for AudioBuffer<i16> {
-    const RATE: f64 = 44_100.0;
+    const RATE: u64 = 44_100;
 
-    fn quantity(&self) -> f64 {
-        self.frames() as f64
+    fn quantity(&self) -> u64 {
+        self.frames() as u64
     }
 }
 
@@ -52,7 +53,8 @@ where
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let RateLimitedStream {
             stream,
-            bucket,
+            tokens,
+            last_checked,
             sleep,
         } = self.get_mut();
         if let Some(sleep) = sleep.as_mut() {
@@ -64,11 +66,23 @@ where
         match Pin::new(stream).poll_next(cx) {
             Poll::Ready(None) => Poll::Ready(None),
             Poll::Ready(Some(item)) => {
-                if let Err(required_tokens) = bucket.acquire(item.quantity()) {
+                let now = SystemTime::now();
+                *tokens += (now
+                    .duration_since(*last_checked)
+                    .unwrap_or_default()
+                    .as_millis() as i128)
+                    .checked_mul(S::Item::RATE as i128)
+                    .unwrap_or(S::Item::RATE as i128 * 3_000)
+                    / 1000
+                    - item.quantity() as i128;
+                *last_checked = now;
+
+                if *tokens < 0 {
                     // Wake up a bit early to give processing time
-                    let duration = ((required_tokens / S::Item::RATE * 1.0e6) as u64)
+                    let duration = u64::try_from(-*tokens / S::Item::RATE as i128 * 1000)
+                        .unwrap_or_default()
                         .checked_sub(100)
-                        .unwrap_or(0);
+                        .unwrap_or_default();
                     let mut s = Box::pin(tokio::time::sleep(Duration::from_millis(duration)));
                     let _ = s.poll_unpin(cx);
                     *sleep = Some(s);
